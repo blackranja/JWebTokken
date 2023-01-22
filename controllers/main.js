2dac1af2e7e262d3db23b4e878025658214a3e8b
Josphat karanja <josphatranja243@gmail.com>
2023-01-22 10:24:27 +0300

    JWT Tokens

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,23 @@
+//check usernam,password in post(login) request
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
 const login = async (req, res) => {
-    res.send('Fake Login/Register/Signup Route');
+    const { username, password } = req.body
+
+    //mongoose validations
+    //JOI
+    //check in the controller
+
+    if (!username || !password) {
+        throw new CustomAPIError('please provide email and password', 400);
+
+    }
+    const id = new Date().getDate();
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
+
+
+    console.log(username, password);
+    res.status(200).json({ msg: 'user Created', token })
 }
 const dashboard = async (req, res) => {
     const luckNumber = Math.floor(Math.random() * 100);
