d1650d23c19e428e55badbc1779ace6c55859cdc
Josphat karanja <josphatranja243@gmail.com>
2023-01-22 22:39:17 +0300

    Authorization Middleware

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const { UnauthenticatedError } = require('../errors');
+const authenticationMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.StartsWith('Bearer')) {
+        throw new UnauthenticatedError('No Token Provided')
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const { id, username } = decoded;
+        req.user = { id, username }
+        next();
+    } catch (error) {
+        throw new UnauthenticatedError('Not Authorized to access this route');
+    }
+}
+module.exports = authenticationMiddleware;
