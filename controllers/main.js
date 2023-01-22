7ecffa1744a4b4aa730fa7e70a4860a1bf3601d3
Josphat karanja <josphatranja243@gmail.com>
2023-01-22 22:21:46 +0300

    Custom Error Handler

--- controllers/main.js
+++ controllers/main.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error');
 const login = async (req, res) => {
-    const { username, password } = req.body
+    const { username, password, idnumber, adminnumber } = req.body
 
     //mongoose validations
     //JOI
@@ -13,10 +13,10 @@ const login = async (req, res) => {
 
     }
     const id = new Date().getDate();
-    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
+    const token = jwt.sign({ id, username, idnumber, adminnumber }, process.env.JWT_SECRET, { expiresIn: '30d' })
 
 
-    console.log(username, password);
+    console.log(username, password, idnumber, adminnumber);
     res.status(200).json({ msg: 'user Created', token })
 }
 const dashboard = async (req, res) => {
