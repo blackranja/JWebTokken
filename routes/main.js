a3b2e593bcd2d5365dd21efc95da38a350e2f87b
Josphat karanja <josphatranja243@gmail.com>
2023-01-22 23:01:49 +0300

    ExpressJS Middleware

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const { login, dashboard } = require('../controllers/main');
-router.route('/dashboard').get(dashboard);
-router.route('/login').post(login);
-module.exports = router;
+const { BadRequestError } = require('../errors');
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        throw new BadRequestError('Please provide email and password');
+    }
+    const id = new Date().getDate();
+    const token = jwt.Sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    res.status(200).json({ msg: 'User Created', token })
+}
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json(
+        {
+            msg: `Hello,${req.user.username}`,
+            secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+        })
+}
+module.exports = { login, dashboard };
