0e4973cd549b6f59d4a8888ee8a9736fced4602e
Josphat karanja <josphatranja243@gmail.com>
2023-01-22 22:29:09 +0300

    Custom Error Handler

---
+++ errors/unauthenticated.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require('./custom-error');
+const { StatusCodes } = require('http-status-codes');
+class UnauthenticatedError extends CustomAPIError {
+    constructor(message) {
+        super(message);
+        this.StatusCode = StatusCodes.UNAUTHORIZED
+    }
+}
+module.exports = UnauthenticatedError;
